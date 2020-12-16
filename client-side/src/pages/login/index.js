194eda9dd2801eb524a92c1c0ba0da2d47548f67
Cesar Augusto Quinteiro de Carvalho <cesaraugusto.dev@gmail.com>
2020-12-15 23:10:19 -0300

    Implementando rest APIs

--- client-side/src/pages/login/index.js
+++ client-side/src/pages/login/index.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 import "./style-login.css";
 import bestHeroImg from "../../assets/logo.png";
+import api from "../../services/api";
 
 export default function Login() {
+  const [email, setemail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const history = useHistory();
+
+  /* Função que efetua login */
+  async function logar(e) {
+    e.preventDefault();
+    var data = {
+      email: email,
+      password: password,
+    };
+    try {
+      const response = await api.apiHero.post("auth", data);
+      console.log(response);
+      history.push("/home");
+    } catch (error) {
+      alert(error);
+    }
+  }
   return (
     <div className="page-login">
       <div className="card-login">
@@ -10,20 +32,34 @@ export default function Login() {
           <img src={bestHeroImg} alt="" className="logo-login" />
         </div>
         <div className="section-form-login">
-          <div className="form-group">
-            <label htmlFor="">E-mail</label>
-            <input type="text" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="">Password</label>
-            <input type="text" />
-          </div>
-          <div className="form-group">
-            <button className="btn">Logar</button>
-          </div>
+          <form onSubmit={logar}>
+            <div className="form-group-login">
+              <label htmlFor="">E-mail</label>
+              <input
+                type="text"
+                value={email}
+                onChange={(e) => setemail(e.target.value)}
+              />
+            </div>
+            <div className="form-group-login">
+              <label htmlFor="">Password</label>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="form-group-login">
+              <button type="submit" className="btn">
+                Logar
+              </button>
+            </div>
+          </form>
         </div>
         <div className="sectio-footer-login">
-          <a href="../register">Cadastre-se</a>
+          <Link className="link-login" to="/register">
+            Cadastre-se
+          </Link>
         </div>
       </div>
     </div>
